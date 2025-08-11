a154f65929bea542f90ba4312b18d496765a2e2f
Vladabeac28 <dokazovavlada@gmail.com>
2025-08-11 11:57:15 +0300

    Update products.js

--- products.js
+++ products.js
@@ -1,7 +1,14 @@
 
-const products = [
+// Стандартні товари
+const defaultProducts = [
     { id: 1, brand: 'Avon', name: 'Парфуми Avon Luck', price: 500, image: '271911043_310_430.jpg', description: 'Легкий квітковий аромат з нотами жасмину та ванілі.' },
     { id: 2, brand: 'Oriflame', name: 'Крем для обличчя Oriflame', price: 300, image: 'images/oriflame1.jpg', description: 'Зволожуючий крем для щоденного догляду за шкірою.' },
     { id: 3, brand: 'Avon', name: 'Помада Avon True', price: 250, image: 'images/avon2.jpg', description: 'Насичений колір і догляд для ваших губ.' },
     { id: 4, brand: 'Oriflame', name: 'Шампунь Oriflame Nature', price: 200, image: 'images/oriflame2.jpg', description: 'Натуральний шампунь для зміцнення волосся.' }
 ];
+
+// Нові товари з localStorage
+let storedProducts = JSON.parse(localStorage.getItem("products")) || [];
+
+// Загальний масив
+const products = [...defaultProducts, ...storedProducts];
