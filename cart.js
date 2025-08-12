e1ae1a4f0a9135976930da0921513bbc739eb63e
Vladabeac28 <dokazovavlada@gmail.com>
2025-08-12 21:20:40 +0300

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => { 
+document.addEventListener('DOMContentLoaded', () => {
   const cartItemsDiv = document.getElementById('cartItems');
   const totalPriceP = document.getElementById('totalPrice');
   const orderButton = document.getElementById('orderButton');
@@ -16,9 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function updateCartCount() {
     if (cartCountSpan) {
-      // Відображаємо загальну кількість товарів у кошику
-      const totalQuantity = Object.values(quantities).reduce((sum, q) => sum + q, 0);
-      cartCountSpan.textContent = totalQuantity;
+      cartCountSpan.textContent = cart.length;
     }
   }
 
